795d0f3d12e1227236b1a10a9af18c7178a6d506
Monelwa <104169292+Monelwa@users.noreply.github.com>
2022-04-22 15:58:45 +0200

    Add files via upload

---
+++ CircleMain.java
@@ -0,0 +1,40 @@
+
+package za.ac.wsu.driver.s220093652;
+import za.ac.wsu.s220093652.circle;
+   //asavela monelwa      
+
+public class CircleMain {
+    public static void main(string[])args){
+        // creates three circle objects
+        circle c1=new circle();
+        circle c2=new circle();
+        circle c3=new circle();
+        // set values to circle object
+        c1.setradius(15);
+        c1.setx(6);
+        c1.sety(6);
+        
+        c2.setradius(10);
+        c2.setx(8);
+        c2.sety(6);
+        
+      c3.setradius(7);
+      c3.setx(15);
+      c3.sety(17);
+      
+// display the information of the circles
+
+ 
+ public static void
+ displayInformation(circle c){
+     system.out.printIn("radius"+c.getradius()+"center:("+c.getx()+","+c.gety()+")");
+     system.out.printf("area:%2f",c.calculateCircumference());
+     
+ system.out.printf("/ndiameter:%2f",c,calculatediameter();
+ system.out.printin();
+ }
+}       
+        
+        
+       
+   
